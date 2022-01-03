a5736ee6509826a6d962b99f92dc775dcef4703c
Chris Sargent <chris@noah.com>
2022-01-03 11:10:44 +0100

    add 2 react libraries

--- libs/ui-atoms/jest.config.js
+++ libs/ui-atoms/jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  displayName: 'csreactapp',
+  displayName: 'ui-atoms',
   preset: '../../jest.preset.js',
   transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/csreactapp',
+  coverageDirectory: '../../coverage/libs/ui-atoms',
 };
