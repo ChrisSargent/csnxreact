e6af7bda3563ed57b033d44d082f5a2c4f982d75
Chris Sargent <chris@noah.com>
2022-01-03 11:25:02 +0100

    Revert "add 2 react libraries"
    This reverts commit a5736ee6509826a6d962b99f92dc775dcef4703c.

--- apps/csreactapp/jest.config.js
+++ apps/csreactapp/jest.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   displayName: 'csreactapp',
   preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/csreactapp',
